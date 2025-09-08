fb233c61fd5ebc6f8c9e8f8c0f4bcf29a9be2b5e
Abdullah Chaudhary <abdullah.chaudhary1605@gmail.com>
2025-09-09 01:46:45 +0500

    add Navber

---
+++ components/Navitems.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navitems = [
+    {name: "Home", href: "/"},
+    {name: "Companions", href: "/companions"},
+    {name: "My Journey", href: "my-journey"},
+    {name: "Pricing", href: "/pricing"},
+]
+
+const Navitems = () => {
+    const pathname = usePathname()
+  return (
+    <nav className="flex items-center gap-4">
+        {navitems.map((item) => (
+            <Link href={item.href} key={item.name} className={cn(pathname === item.href && "text-primary font-semibold")}>
+                {item.name}
+            </Link>
+        ))}
+    </nav>
+  )
+}
+
+export default Navitems
