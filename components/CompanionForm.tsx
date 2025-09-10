122c349693f9b1aff71f87dff46a5655fffee6d6
Abdullah Chaudhary <abdullah.chaudhary1605@gmail.com>
2025-09-11 02:00:37 +0500

    final commit

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -27,23 +27,27 @@ import { useRouter } from "next/navigation";
 import { useCompanions } from "@/context/CompanoinContext";
 
 const formSchema = z.object({
+    id: z.string().min(1, { message: 'Companion is required.'}),
     name: z.string().min(1, { message: 'Companion is required.'}),
     subject: z.string().min(1, { message: 'Subject is required.'}),
     topic: z.string().min(1, { message: 'Topic is required.'}),
     voice: z.string().min(1, { message: 'Voice is required.'}),
     style: z.string().min(1, { message: 'Style is required.'}),
-    duration: z.coerce.number().min(1, { message: 'Duration is required.'}),
+    color: z.string().min(1, { message: 'Color is required.'}),
+    duration: z.number().min(1, { message: 'Duration is required.'}),
 })
 
 const CompanionForm = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            id: '',
             name: '',
             subject: '',
             topic: '',
             voice: '',
             style: '',
+            color: '#000000',
             duration: 15,
         },
     })
